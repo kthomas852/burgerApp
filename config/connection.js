81fea8ff82eac40162b44eba4b88f8ff8a590a3f
Kyle Thomas <kthomas852@yahoo.com>
2018-11-07 20:46:59 -0700

    jfjf

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-//if(process.env.JAWSDB_URL){
-  var connection = mysql.createConnection({
-    host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "t82l5x0rufbya6cc",
-    password: "cf2ea5xvhq64e7fm",
-    database: "q4eczouqz5fvvwfg"
-  });
-//Commented out for use with Jaws
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
-
+if(process.env.JAWSDB_URL){
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
