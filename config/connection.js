dbc16af313a11b2e83ceb821b3bb51d54348401d
Kyle Thomas <kthomas852@yahoo.com>
2018-11-07 21:13:32 -0700

    weorqpqwer

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,15 @@ var mysql = require("mysql");
 if(process.env.JAWSDB_URL){
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
 
@@ -17,7 +26,7 @@ var connection = mysql.createConnection({
   password: "",
   database: "burgers_db"
 });
-};
+
 
 // Make connection.
 connection.connect(function(err) {
@@ -27,6 +36,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
+};
 // Export connection for our ORM to use.
 module.exports = connection;
